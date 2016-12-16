d0413215731c8c36be1156c3c1c1b2b434146099
Mike Anelli <myke_a@mac.com>
2016-12-16 13:12:15 -0800

    Switch dependency to vapor

--- Package.swift
+++ Package.swift
@@ -4,5 +4,6 @@ let package = Package(
     name: "GraphQLResponder",
     dependencies: [
         .Package(url: "https://github.com/GraphQLSwift/Graphiti.git", majorVersion: 0, minor: 2),
+        .Package(url: "https://github.com/vapor/engine.git", majorVersion: 1, minor: 3),
     ]
 )
